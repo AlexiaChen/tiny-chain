1c6f15f0310a42248f933e463e1d618e52d4dcb6
MathxH <brainfvck@foxmail.com>
2019-04-26 11:13:32 +0800

    [+] Init tiny chain project

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "init tiny chain" << std::endl;
+    return 0;
+}
---
+++ test/tinychain_test.cpp
@@ -0,0 +1,8 @@
+#ifndef BOOST_TEST_MODULE
+#define BOOST_TEST_MODULE
+#include <boost/test/unit_test.hpp>
+#endif 
+
+BOOST_AUTO_TEST_SUITE(tinychain_test)
+
+BOOST_AUTO_TEST_SUITE_END()
